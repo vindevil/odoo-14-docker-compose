773f8fcfd6a467d59c9e988651b4ec4ee0fb8e7f
vindevil <vchen365@gmail.com>
2021-01-17 10:40:45 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 DESTINATION=$1
-# PORT=$2
+PASSWD=$2
+# PORT=$3
 # clone Odoo directory
 git clone --depth=1 https://github.com/vindevil/odoo-14-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
-# set permission
+# change odoo admin password
+sed -i 's/vindevil/'$PASSWD'/g' $DESTINATION/config/odoo.conf  # Substitute 'vindevil' with the real password
+# create mapping folders
 mkdir -p $DESTINATION/pgdata
+mkdir -p $DESTINATION/web-data
+mkdir -p $DESTINATION/addons
+mkdir -p $DESTINATION/config
+# set permission
 sudo chmod -R 777 $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
