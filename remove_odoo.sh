244c2ef3db3d68fb69543716d5eb2bf7295085b5
vindevil <vchen365@gmail.com>
2021-01-17 08:57:50 +0800

    Update remove_odoo.sh

--- remove_odoo.sh
+++ remove_odoo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # completely delete/reset Odoo container & data
 docker-compose down
-rm -rf postgresql
-rm -rf etc/addons
-rm -rf etc/filestore
-rm -rf etc/sessions
-rm -rf etc/odoo-server.log
-echo 'Odoo deleted!'
+rm -rf pgdata
+rm -rf web-data
+rm -rf addons
+rm -rf etc
+echo 'Odoo deleted completely!'
