9ec9c38d45ace23f3ea3cbec71c1472e090e63fa
vindevil <vchen365@gmail.com>
2021-01-17 09:51:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 DESTINATION=$1
-PORT=$2
+# PORT=$2
 # clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-14-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/vindevil/odoo-14-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 # set permission
-mkdir -p $DESTINATION/postgresql
+mkdir -p $DESTINATION/pgdata
 sudo chmod -R 777 $DESTINATION
 # config
 if grep -qF "fs.inotify.max_user_watches" /etc/sysctl.conf; then echo $(grep -F "fs.inotify.max_user_watches" /etc/sysctl.conf); else echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf; fi
 sudo sysctl -p
-sed -i 's/10014/'$PORT'/g' $DESTINATION/docker-compose.yml
+#sed -i 's/10014/'$PORT'/g' $DESTINATION/docker-compose.yml  # Substitute 10014 with port number input
 # run Odoo
-docker-compose -f $DESTINATION/docker-compose.yml up -d
+sudo docker-compose -f $DESTINATION/docker-compose.yml up -d
 
-echo 'Started Odoo @ http://localhost:'$PORT
+echo 'Odoo started @ '$PORT' without binding to host.'
